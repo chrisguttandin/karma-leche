a7a84926d2faaa83a8dec626fddc1bb159295d0d
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-15 15:20:50 +0200

    apply linting rules

--- src/module.js
+++ src/module.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
+// eslint-disable-next-line padding-line-between-statements
 const LECHE = path.join(path.dirname(require.resolve('leche')), '../dist/leche.js');
-
 const initLecheFramework = (files) => files.unshift({
     included: true,
     pattern: LECHE,
