53afbec4574c414ff9dc1a7f1c15ea348fa2d585
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-06-19 02:37:28 +0200

    style: run prettier

--- test/unit/module.js
+++ test/unit/module.js
@@ -2,9 +2,7 @@ const lecheFramework = require('../../src/module.js');
 const path = require('path');
 
 describe('karma-leche', () => {
-
     describe('initLeche()', () => {
-
         let initLecheFramework;
 
         beforeEach(() => {
@@ -12,20 +10,21 @@ describe('karma-leche', () => {
         });
 
         it("should unshift leche's dist file", () => {
-            const files = [ 'a fake file' ];
+            const files = ['a fake file'];
 
             initLecheFramework(files);
 
             expect(path.relative(__dirname, files[0].pattern)).to.equal('../../node_modules/leche/dist/leche.js');
 
-            expect(files).to.deep.equal([ {
-                included: true,
-                pattern: files[0].pattern,
-                served: true,
-                watched: false
-            }, 'a fake file' ]);
+            expect(files).to.deep.equal([
+                {
+                    included: true,
+                    pattern: files[0].pattern,
+                    served: true,
+                    watched: false
+                },
+                'a fake file'
+            ]);
         });
-
     });
-
 });
